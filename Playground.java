60d810656d22db3c858e1aae47301911968d44af
Anup Jaiswal <150141325+anupjais@users.noreply.github.com>
2024-05-10 22:57:20 +0530

    Some updates

--- Playground.java
+++ Playground.java
@@ -2,7 +2,14 @@ class Playground
 {
 	public static void main(String[] args)
 	{
-		Accounts acc = new Accounts();
-		acc.changePin();	
+		// Accounts acc = new Accounts();
+		// acc.changePin();	
+
+		// int [] arr = {1,2,3,4,5,6};
+		String [] arr = {"Ram","Shyam","Harish", "Girish"};
+		for(int i = 0; i<arr.length; i++)
+			System.out.println((i+1)+". "+arr[i]);
+
+		System.out.println("Bye");
 	}
 }
